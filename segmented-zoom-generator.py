89053e41c50026b8b50437e48b63eab2ea3b39c7
wonkybadonk <zthurman@users.noreply.github.com>
2015-01-08 17:48:52 -0800

    Update segmented-zoom-generator.py

--- segmented-zoom-generator.py
+++ segmented-zoom-generator.py
@@ -1,5 +1,5 @@
 from __future__ import division
-from mandelbrot import generate_viewport, gen_palette
+from mandelbrot_cstyle import generate_viewport, gen_palette
 import md5
 import colorsys
 from moviepy.video.VideoClip import VideoClip
@@ -44,32 +44,16 @@ class Mandelbrot:
             color = colorsys.hls_to_rgb(h, l, s)
             palette.append(color)
 
-        """
-        for i in xrange(cycle_size):
-            m = (i % cycle_size)/cycle_size
-            if m < 0.5:
-                h = 240 # blue
-                # black to white
-                l = m * 200
-            else:
-                h = 30 # orange
-                l = 100 - (m - 0.5 * 200)
-
-            log.warn("H: {} S: {} L: {} M:".format(h, 0.5, l, m))
-            r, g, b = colorsys.hls_to_rgb(h/255, l/100, 0.5)
-
-            palette.append((r*255, g*255, b*255))
-        """
         palette = [(np.uint8(r*255), np.uint8(g*255), np.uint8(b*255)) for r, g, b in palette]
         log.warn("Using palette: {}".format(palette))
         return palette
 
-    def gen_viewport(self, x, y, z):
+    def gen_viewport(self, u, v, z):
         if not self.palette:
             # self.palette = gen_palette()
             self.palette = self.javascript_palette()
 
-        return generate_viewport(x, y, z, dpu=self.dpu, w=self.w, h=self.h, max_i=self.max_i, palette=self.palette)
+        return generate_viewport(u, v, z, dpu=self.dpu, w=self.w, h=self.h, max_i=self.max_i, palette=self.palette)
 
     def cache_key(self, x, y, z):
         return md5.new('{}{}{}'.format(x, y, z)).digest()
@@ -86,7 +70,7 @@ class MandelbrotClip:
     def fly_to(self, x0, y0, x1, y1):
         pass
 
-    def make_index_to(self, x, y, start_z, end_z):
+    def make_index_to(self, u, v, start_z, end_z):
         def gen_frame(t):
             log.debug("Making frame at t={}".format(t))
             if self.duration:
@@ -95,14 +79,13 @@ class MandelbrotClip:
             else:
                 z_t = 0
 
-            return self.mandelbrot.gen_viewport(x, y, z_t)
+            return self.mandelbrot.gen_viewport(u, v, z_t)
 
         self.videoclip = self.videoclip.set_make_frame(gen_frame)
         pass
 
 
 video = MandelbrotClip(size=(1280, 720), duration=20)
-
 #video.make_index_to(-1.772, 0.013, -5, 15)
 #video.make_index_to(-0.75538, 0.11220, -3, 15)
 video.make_index_to(0.3750001200618655, -0.2166393884377127, -3, 15)
@@ -110,4 +93,12 @@ video.make_index_to(0.3750001200618655, -0.2166393884377127, -3, 15)
 #video.make_index_to(0.001643721971153, 0.822467633298876, -3, 50)
 #video.make_index_to(-0.75, -.1, -1.5, 20)
 video.videoclip.write_videofile('RealZoom.mp4', fps=25, preset='medium', threads=3)
+#video.make_index_to(-1.772, 0.013, -3, 5)
+# Not quite:
+# video.make_index_to(-0.75, -.1, -1.5, 10)
+# -767, -104, 10
+#from mandelbrot import transform_index
+#u, v = transform_index(-767, -104, 10)
+#video.make_index_to(u, v, -1.5, 10)
+#video.videoclip.write_videofile('out1.mp4', fps=60, codec='libx264', preset='ultrafast', threads=2)
 #video.videoclip.write_images_sequence(nameformat='output/frame%03d.png', fps=5)
