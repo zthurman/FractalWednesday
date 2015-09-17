effbfd615ed05036635a57439619e14aa5567a08
zthurman <zrt81491@gmail.com>
2015-09-16 21:33:50 -0700

    made numpy explicit and if name == mained it

--- PerturbedMandelbrot.py
+++ PerturbedMandelbrot.py
@@ -1,21 +1,24 @@
 #!/usr/bin/env python
 # Perturbation theory implementation of the Mandelbrot set
 
-from numpy import *
+# Copyright (C) 2015 Zechariah Thurman
+# GNU GPLv2
+
 import pylab
 import matplotlib.pyplot as plt
+import numpy as np
 
 
 def mandelbrot( h, w, maxit=35):
      # Returns an image of the Mandelbrot fractal of size (h,w). Lower the maxit count, the less detail in the
      # set. Increase maxit for more details.
 
-    #y,x = ogrid[ -1.4:1.4:h*1j, -2:0.8:w*1j ]		# complex plane for set to set
-    #y,x = np.mgrid[ -1.4:1.4:h*1j, -2:0.8:w*1j ]
+    #y,x = ogrid[ -1.4:1.4:h*1j, -2:0.8:w*1j ]		# complex plane for set to set on, either ogrid or mgrid work
+    y,x = np.mgrid[ -1.4:1.4:h*1j, -2:0.8:w*1j ]
     c = x+y*1j			# initialized c value
     z = c		# first z value intialized to c
 
-    divtime = maxit + zeros(z.shape, dtype=int)		# time of divergence
+    divtime = maxit +  np.zeros(z.shape, dtype=int)		# time of divergence
     an = 0 + 0j		# a initial value, hint came from: http://math.stackexchange.com/questions/939270/perturbation-of-mandelbrot-set-fractal
     bn = 0 + 0j		# b initial value, although these values really don't matter
     cn = 0 + 0j		# c initial value, like at all
@@ -43,16 +46,16 @@ def mandelbrot( h, w, maxit=35):
         # c = 0.001643721971153 - 0.822467633298876j
 
         yn = dn + z				# perturbed Mandel yn solns
-        diverge = ((yn)*(conj(yn))).real > 25**25     # who is diverging, term on the right of the inequality determines level of detail
+        diverge = ((yn)*(np.conj(yn))).real > 25**25     # who is diverging, term on the right of the inequality determines level of detail
         div_now = diverge & (divtime==maxit)  # who is diverging now
         divtime[div_now] = i                  # note when
         z[diverge] = 2                       # avoid diverging too much
     return divtime
 
-
-plt.figure(num=None, figsize=(10, 10), dpi=80, facecolor='w', edgecolor='k')
-pylab.imshow(mandelbrot(500,500), cmap='bone_r')
-pylab.title('The Mandelbrot Viewer')
-pylab.xlabel('Real Values')
-pylab.ylabel('Imaginary Values')
-pylab.show()
+if __name__ == "__main__":
+    plt.figure(num=None, figsize=(10, 10), dpi=80, facecolor='w', edgecolor='k')
+    pylab.imshow(mandelbrot(500,500), cmap='bone_r')
+    pylab.title('The Mandelbrot Viewer')
+    pylab.xlabel('Real Values')
+    pylab.ylabel('Imaginary Values')
+    pylab.show()
